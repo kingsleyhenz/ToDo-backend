aa35fd044453334ed9a3a99a37aeff209354a27e
BLACKHENZ29 <ayehenz29@gmail.com>
2023-02-09 15:49:24 +0000

    Debugging changes

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.use(cors({
     origin: '*'
 }
 ))
+app.options('*', cors())
+app.use(
+    cors({
+        credentials: true,
+        origin: true,
+        allowedHeaders: "*"
+    })
+)
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
 app.use("/api/v1/task", todoRoute)
 
 const PORT = process.env.PORT || 10000

7ba6c8f1e6f97dddab9a76fc89e3506b0c6d69f3
BLACKHENZ29 <ayehenz29@gmail.com>
2023-02-10 13:26:52 +0000

    Trying different things to solve the error

--- controller/todoController.js
+++ controller/todoController.js
@@ -5,7 +5,7 @@ import Task from "../models/todomodel.js";
 export const addTask = async (req, res) => {
   const {head, item, category, status, startDate, endDate} = req.body;
   try{
-      const foundTask = await Task.findOne({item})
+      const foundTask = await Task.findOne({user: req.userAuth._id})
       if(!foundTask){
           const task = await Task.create({
             head,
@@ -14,7 +14,7 @@ export const addTask = async (req, res) => {
             status,
             startDate,
             endDate,
-            user: req.userAuth
+            user: req.userAuth._id
           })
           res.json({
               status: "success",

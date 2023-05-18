e61e71e9d42e01b27bcd7297ac96c5927ed46513
BLACKHENZ29 <ayehenz29@gmail.com>
2023-05-18 11:58:18 +0100

    route established

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,20 +1,23 @@
 import  Express  from "express";
-import { addTask, allTasks,deleteTask} from "../controller/todoController.js";
-import { Register, Login, getUser } from "../controller/regController.js";
+import { addTask, allTasks, deleteTask} from "../controller/todoController.js";
+import { Register, Login, getUser, updateUserProfile } from "../controller/regController.js";
 import {isLogin} from "../middleware/LoggedIn.js";
+
 
 const todoRoute = Express.Router();
 
 
-todoRoute.post("/register",Register)
+todoRoute.post("/register",Register);
 
-todoRoute.post("/login", Login)
+todoRoute.post("/login", Login);
 
-todoRoute.post("/create",isLogin, addTask)
+todoRoute.post("/create",isLogin, addTask);
 
 todoRoute.get("/allTasks",isLogin, allTasks);
 
 todoRoute.get("/getUser", isLogin, getUser);
+
+todoRoute.patch("/updateUser", isLogin, updateUserProfile);
 
 todoRoute.delete("/:id", isLogin, deleteTask);
 
